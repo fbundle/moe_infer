cfca8bd3470ddb246d59cfebf09d8011e75f108f
nguyen ngoc khanh <khanh.nguyen.contact@gmail.com>
2026-05-26 01:42:21 +0800

    auto_commit

--- helpers/avail_models.py
+++ helpers/avail_models.py
@@ -5,6 +5,6 @@ To activate/deactivate engines, add or remove them from the list below.
 
 # Currently active engine(s)
 ACTIVE_ENGINES = [
-    "Qwen35MoEFused4bitExp2",
+    # "Qwen35MoEFused4bitExp2",
     "Qwen35MoEBq4Exp2",
 ]
